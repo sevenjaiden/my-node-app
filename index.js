24b1efb2a3e868a0528e1d10fcb22758c5893e90
sevenjaiden <157500764+sevenjaiden@users.noreply.github.com>
2024-10-28 18:22:17 -0400

    commit2

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 
-console.log("hi");
+// Import the express library and assign it to a variable
+import express from 'express'
+
+// Create an instance of an express application 
+const app = express()
+
+// Set the port the application will be running on
+const port = process.env.PORT || 3001
+
+// Set up a response for the root path of the application
+app.get('/', (req, res) => {
+  res.send("ya this is node ya")
+})
+
+// Set the application to listen a port
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}` )
+})
+
+const movies = {
+    horror: "overlord",
+    comedy: "spongebob"
+}
+
+app.get('/country', (req, res) => {
+    const countryName = req.query.country; // Access the country name
+    res.send(`Country: ${countryName}`);
+});
+
+app.get('/movies', (req, res) => {
+    const genreName = req.query.genre; // Access the genre name
+    if (!movies[genreName]) {
+	    res.send(`no movies found!`);
+    }
+    res.send(`genre: ${genreName}, ${movies[genreName]}`);
+});
